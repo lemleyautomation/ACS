49fa2c275e73b990581bbbd0f53390ce4dd3f210
lemleyautomation <lemley.automation@gmail.com>
2021-03-18 10:39:40 -0400

    new file:   build/CMakeFiles/3.13.4/CMakeCCompiler.cmake 	new file:   build/CMakeFiles/3.13.4/CMakeCXXCompiler.cmake 	new file:   build/CMakeFiles/3.13.4/CMakeDetermineCompilerABI_C.bin 	new file:   build/CMakeFiles/3.13.4/CMakeDetermineCompilerABI_CXX.bin 	new file:   build/CMakeFiles/3.13.4/CMakeSystem.cmake 	new file:   build/CMakeFiles/3.13.4/CompilerIdC/CMakeCCompilerId.c 	new file:   build/CMakeFiles/3.13.4/CompilerIdC/a.out 	new file:   build/CMakeFiles/3.13.4/CompilerIdCXX/CMakeCXXCompilerId.cpp 	new file:   build/CMakeFiles/3.13.4/CompilerIdCXX/a.out 	new file:   build/CMakeFiles/ACS.dir/CXX.includecache 	new file:   build/CMakeFiles/ACS.dir/DependInfo.cmake 	new file:   build/CMakeFiles/ACS.dir/build.make 	new file:   build/CMakeFiles/ACS.dir/cmake_clean.cmake 	new file:   build/CMakeFiles/ACS.dir/depend.internal 	new file:   build/CMakeFiles/ACS.dir/depend.make 	new file:   build/CMakeFiles/ACS.dir/flags.make 	new file:   build/CMakeFiles/ACS.dir/link.txt 	new file:   build/CMakeFiles/ACS.dir/progress.make 	new file:   build/CMakeFiles/ACS.dir/src/Main.cpp.o 	new file:   build/CMakeFiles/ACS.dir/src/VisionAlgorithm.cpp.o 	new file:   build/CMakeFiles/CMakeDirectoryInformation.cmake 	new file:   build/CMakeFiles/CMakeError.log 	new file:   build/CMakeFiles/CMakeOutput.log 	new file:   build/CMakeFiles/Makefile.cmake 	new file:   build/CMakeFiles/Makefile2 	new file:   build/CMakeFiles/TargetDirectories.txt 	new file:   build/CMakeFiles/cmake.check_cache 	new file:   build/CMakeFiles/feature_tests.bin 	new file:   build/CMakeFiles/feature_tests.c 	new file:   build/CMakeFiles/feature_tests.cxx 	new file:   build/CMakeFiles/progress.marks

---
+++ build/CMakeFiles/ACS.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ACS.dir/src/Main.cpp.o"
+  "CMakeFiles/ACS.dir/src/VisionAlgorithm.cpp.o"
+  "ACS.pdb"
+  "ACS"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ACS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
